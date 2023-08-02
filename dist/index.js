352875b7ce8f92c0b63d8c24af3334faeb836d2b
victor magdaleno <vrmq@gft.com>
2023-08-02 10:51:56 -0600

    type narrowing

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,98 @@
 "use strict";
-//* generics
-const numbs = [];
-const colors = [];
-const $name = document.querySelector('#username');
-console.dir($name);
-const $btn = document.querySelector('btn');
-//* custom generic
-const numberIdentity = (item) => {
-    return item;
-};
-const stringIdentity = (item) => {
-    return item;
-};
-const booleanIdentity = (item) => {
-    return item;
-};
-/* const identity = (item: any): any => {
-  return item;
-} */
-function identity(item) {
-    return item;
+//* type narrowing
+//? guard type of
+function triple(value) {
+    typeof value === 'string' ? value.repeat(3) : value * 3;
 }
-identity;
-identity(true);
-identity(7);
-identity({ nickname: 'Raven', strength: 8700 });
-function getRandomElement(list) {
-    const randIndex = Math.floor(Math.random() * list.length);
-    return list[randIndex];
+//? truthiness guards
+const $el = document.getElementById('cta');
+if ($el) {
+    $el;
 }
-getRandomElement(['a', 'b', 'c', 'd']);
-getRandomElement([19, 23, 34]);
+else {
+    $el;
+}
+const getAssets = (asset) => {
+    if (asset) {
+        for (const char of asset) {
+            console.log(char);
+        }
+    }
+    else {
+        console.log('You didnt pass in a word');
+    }
+};
+//? Equality narrowing
+const someDemo = (x, y) => {
+    if (x === y) {
+        x.toLowerCase();
+    }
+};
+someDemo('3', false);
+const getRunTime = (media) => {
+    if ('numChapters' in media) {
+        const { numChapters, episodeLength } = media;
+        return numChapters * episodeLength;
+    }
+    return media.duration;
+};
+//console.log(getRunTime({ title: 'Gladiator', duration: 150 }));
+/* console.log(
+  getRunTime({ title: 'Yugioh', numChapters: 124, episodeLength: 20 })
+); */
+//? Instanceof
+const printFullDate = (date) => {
+    date instanceof Date
+        ? console.log(date.toUTCString())
+        : console.log(new Date(date).toUTCString());
+};
+class User {
+    constructor(userName) {
+        this.userName = userName;
+    }
+}
+class Company {
+    constructor(name) {
+        this.name = name;
+    }
+}
+function printName(entity) {
+    if (entity instanceof User) {
+        console.log(entity.userName);
+    }
+    else {
+        console.log(entity.name);
+    }
+}
+const isCat = (animal) => {
+    return animal.lives !== undefined;
+};
+const makeNoise = (animal) => {
+    if (isCat(animal)) {
+        return 'Meow';
+    }
+    else {
+        return 'Wooaf';
+    }
+};
+const getFarmSoundAnimal = (animal) => {
+    switch (animal.kind) {
+        case 'rooster':
+            return 'Kikiri ki';
+        case 'cow':
+            return 'Muuuuu';
+        case 'pig':
+            return 'oink oink';
+        default:
+            const mustAssignANewCase = animal;
+            return mustAssignANewCase;
+    }
+};
+const lola = {
+    name: 'Lola',
+    weight: 1,
+    age: '2',
+    kind: 'cow'
+};
+const sound = getFarmSoundAnimal(lola);
+console.log(sound);
---
+++ dist/multiple-generic-types.js
@@ -0,0 +1,45 @@
+"use strict";
+//*multiples generic types
+function merge(object1, object2) {
+    return Object.assign(Object.assign({}, object1), object2);
+}
+const comboObj = merge({ name: 'Robin' }, { pets: [18, 8] });
+// Can type parameters as a generic type or with an interface
+function printPower(power) {
+    return power.power * 2;
+}
+function printPower2(power) {
+    return power.power * 2;
+}
+//* Default types
+// with = assing a generic as a primitive by default
+function makeListEmpty() {
+    return [];
+}
+const chars = makeListEmpty();
+const bools = makeListEmpty();
+class VideoPlaylist {
+    constructor() {
+        this.videos = [];
+    }
+}
+class SongPlaylist {
+    constructor() {
+        this.songs = [];
+    }
+}
+class Playlist {
+    constructor() {
+        this.queu = [];
+    }
+    add(item) {
+        this.queu.push(item);
+    }
+    remove(ele) {
+        return this.queu.filter((item) => item !== ele);
+    }
+}
+const songs = new Playlist();
+const videos = new Playlist();
+// videos.remove()
+// songs.add()
